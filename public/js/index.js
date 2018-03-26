1efed7916f407ab26fb40f4e1d759232ece775bb
weed <playvrt@gmail.com>
2018-03-26 21:33:02 +0700

    su tai na ja

--- public/js/index.js
+++ public/js/index.js
@@ -15,14 +15,13 @@ function sendChangeVidCommand() {
 
 
   // problem
+
   var group = document.querySelector('.group:checked').value;
   var client = document.querySelector('.client:checked').value;
 
-
-
+  
   var command = document.getElementById("vidForm").elements.item(0).value;
   var data = document.getElementById("vidForm").elements.item(1).value;
-
   var msg2 = { group_id: group, client_id: client, command: command, data: data };
 
   socket.emit("sendCommand", msg2);
