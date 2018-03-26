c7a7e42d491bda7d38ec38db9496033f001f901e
Prachpawee Tangpaiboon <5831305030@lamduan.mfu.ac.th>
2018-03-26 19:07:08 +0700

    update command2

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,25 @@
 var mainText = document.getElementById("mainText");
 var submitBtn = document.getElementById("submitBtn");
 
-
 function submitClick() {
-    var firebaseRef = firebase.database().ref();
-    firebaseRef.child("Text").set("some value");
+  var firebaseRef = firebase.database().ref();
+  firebaseRef.child("Text").set("some value");
 }
 
-function sendJson() {
-    var command = document.getElementById("dataForm").elements.item(0).value;
-    var data = document.getElementById("dataForm").elements.item(1).value;
-    var msg2 = { 'group_id': 0, 'client_id': 0, 'command': command, 'data': data };
-  
-    socket.emit("sendCommand", msg2)
-    console.log(msg2)
+function sendChangeVidCommand() {
+  var command = document.getElementById("vidForm").elements.item(0).value;
+  var data = document.getElementById("vidForm").elements.item(1).value;
+  var msg2 = { group_id: 0, client_id: 0, command: command, data: data };
 
+  socket.emit("sendCommand", msg2);
+  console.log(msg2);
 }
 
+function sendChangeTemCommand() {
+  var command = document.getElementById("temForm").elements.item(0).value;
+  var data = document.getElementById("temForm").elements.item(1).value;
+  var msg2 = { group_id: 0, client_id: 0, command: command, data: data };
+
+  socket.emit("sendCommand", msg2);
+  console.log(msg2);
+}
